44782974977198d285943c387ef5e878b48d3df8
smamgain123 <darwinshaleen@gmail.com>
2026-07-14 22:23:36 -0400

    feat: add watchlist visibility toggle

--- services/watchlist_service.py
+++ services/watchlist_service.py
@@ -18,7 +18,7 @@ class NotInWatchlistError(Exception):
     """Raised when trying to remove a film that is not in the watchlist."""
     pass
 
-def add_to_watchlist(user_id, film_id):
+def add_to_watchlist(user_id, film_id, public=False):
     """
     Save a film to a user's watchlist.
 
@@ -46,7 +46,11 @@ def add_to_watchlist(user_id, film_id):
             f"Film '{film_id}' is already in this user's watchlist"
         )
     
-    entry = WatchlistEntry(user_id=user_id, film_id=film_id)
+    entry = WatchlistEntry(
+    user_id=user_id,
+    film_id=film_id,
+    public=public,
+)
     db.session.add(entry)
     db.session.commit()
     return entry
